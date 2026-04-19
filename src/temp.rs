a4050bd5e18f9c6ea9b8f0135f65af3675318d0f
cupkejk <czarekbgs12@gmail.com>
2026-04-19 20:08:18 +0200

    deleted temp file

--- src/temp.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let cities: Vec<[f32; 2]> = Vec::new();  // cities jest właścicielem danych
-    let tour = cities;  // przeniesienie własności (move)
-    // cities już nie istnieje — kompilator zgłosi błąd przy próbie użycia!
-}
